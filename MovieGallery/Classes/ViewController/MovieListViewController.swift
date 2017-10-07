8a741bbd5ea9424ffbbd8b1be64d3dd4adf3dc01
rendyhanantak <rendy.hananta@tiket.com>
2017-10-07 17:33:54 +0700

    init pod

---
+++ MovieGallery/Classes/ViewController/MovieListViewController.swift
@@ -0,0 +1,46 @@
+//
+//  MovieListViewController.swift
+//  MovieGallery
+//
+//  Created by Rendy Hananta on 07/10/17.
+//  Copyright © 2017 Rendy Hananta. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    private func setupView() {
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
+//MARK: - UITableViewDataSource
+extension MovieListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        return cell
+    }
+}
+
+//MARK : - UITableViewDelegate
+extension MovieListViewController: UITableViewDelegate {
+    
+}
